f052efee8f8b15890dd670245b75bc30913e6377
mary <mm@mail.ru>
2023-03-10 09:17:50 +0300

    HW Task 1_1 changes

--- Lesson_1/HW/1_1/Program.cs
+++ Lesson_1/HW/1_1/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 1. Напишите программу, которая на вход принимает два числа и выдаёт,
+//    какое число большее, а какое меньшее.
+
+string s_n_1 = Console.ReadLine()!;
+string s_n_2 = Console.ReadLine()!;
+
+int n_1 = int.Parse(s_n_1);
+int n_2 = int.Parse(s_n_2);
+
+if (n_1 > n_2)
+{
+    Console.Write($"The number {n_1} is greater than {n_2}");
+}
+else if (n_1 == n_2)
+{
+    Console.Write($"The number {n_1} is equal to the number {n_2}");
+}
+else
+{
+    Console.Write($"The number {n_2} is greater than {n_1}");
+}
